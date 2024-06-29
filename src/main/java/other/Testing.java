3c418e1cf079928ce942f78f54c72817475bdca9
praveen <praveen.yadahalli@proverne.com>
2024-06-30 05:19:04 +0530

    testing changes for new file addition

---
+++ src/main/java/other/Testing.java
@@ -0,0 +1,10 @@
+package other;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Testing Git configuration");
+	}
+
+}

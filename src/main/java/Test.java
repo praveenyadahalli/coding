ea2ccaf2a1fc9b76066bb877708ae2b45c926c05
praveen yadahalli <praveenya923@gmail.com>
2024-11-28 21:51:10 +0530

    hello

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		solve(sc);
+	}
+	
+	public static void solve(Scanner sc) {
+		
+	} 
+
+}

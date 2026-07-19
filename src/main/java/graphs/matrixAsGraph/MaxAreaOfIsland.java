21768cf831631357ddfd12e82fdf0f7cf4e1089a
Praveen Yadahalli <58703548+praveenyadahalli@users.noreply.github.com>
2026-07-19 09:52:10 +0530

    minor changes

--- src/main/java/graphs/matrixAsGraph/MaxAreaOfIsland.java
+++ src/main/java/graphs/matrixAsGraph/MaxAreaOfIsland.java
@@ -4,6 +4,49 @@ import java.util.*;
 
 public class MaxAreaOfIsland {
 	public static void main(String[] args) {
+		int[][] grid1 = {
+				{0,0,1,0,0,0,0,1,0,0,0,0,0},
+				{0,0,0,0,0,0,0,1,1,1,0,0,0},
+				{0,1,1,0,1,0,0,0,0,0,0,0,0},
+				{0,1,0,0,1,1,0,0,1,0,1,0,0},
+				{0,1,0,0,1,1,0,0,1,1,1,0,0},
+				{0,0,0,0,0,0,0,0,0,0,1,0,0},
+				{0,0,0,0,0,0,0,1,1,1,0,0,0},
+				{0,0,0,0,0,0,0,1,1,0,0,0,0}};
 		
+		int[][] grid2 = {{0,0,0,0,0,0,0,0}};
+		
+		System.out.println(maxAreaOfIsland(grid2));
+	}
+
+
+	public static int maxAreaOfIsland(int[][] grid) {
+		int m = grid.length;
+		int n = grid[0].length;
+
+		int max = 0;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (grid[i][j] == 1) {
+					int count = 0;
+					count = dfs(grid, i, j, m, n, count);
+					max = Math.max(count, max);
+				}
+			}
+		}
+		return max;
+	}
+
+	public static int dfs(int[][] grid, int i, int j, int m, int n, int count) {
+		if (i > m - 1 || j > n - 1 || i < 0 || j < 0 || grid[i][j] == 0) {
+			return count;
+		}
+		count++;
+		grid[i][j] = 0;
+		count=dfs(grid, i + 1, j, m, n, count);
+		count=dfs(grid, i, j + 1, m, n, count);
+		count=dfs(grid, i - 1, j, m, n, count);
+		count=dfs(grid, i, j - 1, m, n, count);
+		return count;
 	}
 }
---
+++ src/main/java/graphs/matrixAsGraph/NumberOfIslands.java
@@ -0,0 +1,46 @@
+package graphs.matrixAsGraph;
+
+// leetcode:200
+public class NumberOfIslands {
+	public static void main(String[] args) {
+		char[][] grid1 = { { '1', '1', '1', '1', '0' }, 
+				{ '1', '1', '0', '1', '0' }, 
+				{ '1', '1', '0', '0', '0' },
+				{ '0', '0', '0', '0', '0' } };
+		
+		char[][] grid2 = { { '1', '1', '0', '0', '0' }, 
+				{ '1', '1', '0', '0', '0' }, 
+				{ '0', '0', '1', '0', '0' },
+				{ '0', '0', '0', '1', '1' } };
+		
+		System.out.println(numIslands(grid1));
+	}
+
+	public static int numIslands(char[][] grid) {
+		int m = grid.length;
+		int n = grid[0].length;
+
+		int count = 0;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (grid[i][j] != '0') {
+					count++;
+					dfs(grid, i, j, m, n);
+				}
+			}
+		}
+		return count;
+	}
+
+	private static void dfs(char[][] grid, int i, int j, int m, int n) {
+		if (i < 0 || j < 0 || i > m - 1 || j > n - 1 || grid[i][j] == '0') {
+			return;
+		}
+		
+		grid[i][j]='0';
+		dfs(grid, i + 1, j, m, n);
+		dfs(grid, i, j + 1, m, n);
+		dfs(grid, i - 1, j, m, n);
+		dfs(grid, i, j - 1, m, n);
+	}
+}
